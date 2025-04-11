ece685a21d6c9f9777e2d5b18284b3d94eb95a51
Danika Peet <DPeet@lcs.on.ca>
2025-04-10 20:46:34 -0400

    Initial Commit

---
+++ Pebble/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pebble
+//
+//  Created by Danika Peet on 2025-04-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Rectangle()
+                .frame(width: 600, height: 1000)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pebble/PebbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PebbleApp.swift
+//  Pebble
+//
+//  Created by Danika Peet on 2025-04-10.
+//
+
+import SwiftUI
+
+@main
+struct PebbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
